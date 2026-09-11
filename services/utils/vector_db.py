b15205826b8aa03526ff8986b5bb35b4d8c3d884
avinash24112007 <avinash24112007@gmail.com>
2026-09-11 10:42:05 +0530

    replace integer id's with uuid id's for chunk points

--- services/utils/vector_db.py
+++ services/utils/vector_db.py
@@ -1,3 +1,5 @@
+import uuid
+
 from qdrant_client import QdrantClient
 from qdrant_client.models import Fusion, FusionQuery, Prefetch, SparseVector, Distance, VectorParams, PointStruct, SparseVectorParams
 from typing import TYPE_CHECKING, List, Dict, Any, Optional
@@ -127,7 +129,7 @@ class QdrantStorage:
                 chunk_metadata.update(chunk.metadata)
             
             point = PointStruct(
-                id=i,
+                id=uuid.uuid4(),
                 vector={
                     "dense": dense_embedding,
                     "sparse": SparseVector(
