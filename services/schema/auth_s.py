786634bd71c33448f5ad12775423cdddfa401d11
mahekbhandari007-lab <mahekbhandari007@gmail.com>
2026-09-11 10:04:25 +0530

    Update auth_s.py

--- services/schema/auth_s.py
+++ services/schema/auth_s.py
@@ -1,29 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
+
 class pyd_login(BaseModel):
-    email:str
-    passcode:str
+    email: EmailStr
+    password: str
 
 
 class pyd_register(BaseModel):
-    UUser_ID :int
-    user_name : str
-    email:str
-    passcode: str
-    Role:str
-
-
-class UserCreate(BaseModel):
     user_name: str
-    email: str
+    email: EmailStr
     password: str
-    Role: str
+    Role: str = "user"  # Defaults to 'user' if not specified
+
 
 class ChatCreate(BaseModel):
     Chat_name: str
     User_Id: int
     Human_message: str
-    AI_message: str | None = None
-    Upload_Doc: str | None = None
-    
+    AI_message: Optional[str] = None
+    Upload_Doc: Optional[str] = None
